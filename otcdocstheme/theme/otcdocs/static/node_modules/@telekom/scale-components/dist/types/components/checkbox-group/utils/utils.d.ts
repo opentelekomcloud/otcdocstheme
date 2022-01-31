a7146df1d0fc6d74284c74c35e35f93b79538110
Sebastian Gode <sebastian.gode@telekom.de>
2022-01-31 12:55:02 +0000

    Removed unecessarys node stuff

--- otcdocstheme/theme/otcdocs/static/node_modules/@telekom/scale-components/dist/types/components/checkbox-group/utils/utils.d.ts
+++
@@ -1,7 +0,0 @@
-export interface CheckboxState {
-  id: string;
-  checked: boolean;
-  disabled: boolean;
-  indeterminate: boolean;
-}
-export declare const objDiffer: (obj1: CheckboxState[], obj2: CheckboxState[]) => boolean;
