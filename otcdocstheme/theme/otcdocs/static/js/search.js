14b6050b7fbdf7f5f8c5a1d805c441e2ea41eeb3
Sebastian Gode <sebastian.gode@telekom.de>
2022-03-11 08:43:49 +0000

    Changed to search all indexes

--- otcdocstheme/theme/otcdocs/static/js/search.js
+++ otcdocstheme/theme/otcdocs/static/js/search.js
@@ -29,7 +29,7 @@ async function searchRequest(val) {
             }
         }
     };
-    let response = await fetch('https://opensearch-stg.eco.tsi-dev.otc-service.com/test-index/_search', {
+    let response = await fetch('https://opensearch-stg.eco.tsi-dev.otc-service.com/_all/_search', {
         method: 'POST',
         mode: 'cors',
         headers: {
