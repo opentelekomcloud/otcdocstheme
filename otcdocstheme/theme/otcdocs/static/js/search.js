25ebfc216de5e1cf887687ed6a0e95d5541b59fc
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2022-03-14 15:10:56 +0100

    New Prod URL (#67)
    New Prod Search URL
    
    Reviewed-by: kucerakk <kucerakk@gmail.com>
    Reviewed-by: Tino Schr <None>

--- otcdocstheme/theme/otcdocs/static/js/search.js
+++ otcdocstheme/theme/otcdocs/static/js/search.js
@@ -29,7 +29,7 @@ async function searchRequest(val) {
             }
         }
     };
-    let response = await fetch('https://opensearch-stg.eco.tsi-dev.otc-service.com/helpcenter-*/_search', {
+    let response = await fetch('https://opensearch.eco.tsi-dev.otc-service.com/helpcenter-*/_search', {
         method: 'POST',
         mode: 'cors',
         headers: {
