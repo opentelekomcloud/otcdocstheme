45b1ea1479af4ff41eb00bdda5477f026ed9eae4
Sebastian Gode <sebastian.gode@telekom.de>
2022-03-31 08:08:22 +0000

    Added checks for docsportal

--- otcdocstheme/theme/otcdocs/static/js/search.js
+++ otcdocstheme/theme/otcdocs/static/js/search.js
@@ -98,9 +98,15 @@ const createMainResult = (response) => {
         let contentDiv = document.getElementById('docs-content')
         contentDiv.insertAdjacentHTML("afterend", "<div id='searchResultsEnter' class='overflow-hidden'></div>");
         contentDiv.classList.add('nodisplay')
-        document.getElementById('left-sidebar').classList.add('not-visible')
-        document.getElementById('right-sidebar').classList.add('not-visible')
-        document.getElementById('breadcrumbs').classList.add('not-visible')
+        // On docsportal starpage we don't have breadcrumbs or sidebar, so check for that
+        try {
+            document.getElementById('left-sidebar').classList.add('not-visible')
+            document.getElementById('right-sidebar').classList.add('not-visible')
+            document.getElementById('breadcrumbs').classList.add('not-visible')
+        }
+        catch(err) {
+            document.getElementById('left-sidebar').classList.add('not-visible')
+        }
         div = document.getElementById('searchResultsEnter')
     }
 
@@ -221,10 +227,17 @@ const deleteEnterResults = () => {
     if (div) {
         div.parentNode.removeChild(div)
     }
-    document.getElementById('docs-content').classList.remove('nodisplay');
-    document.getElementById('left-sidebar').classList.remove('not-visible')
-    document.getElementById('right-sidebar').classList.remove('not-visible')
-    document.getElementById('breadcrumbs').classList.remove('not-visible')
+    // On docsportal starpage we don't have breadcrumbs or sidebar, so check for that
+    try {
+        document.getElementById('docs-content').classList.remove('nodisplay');
+        document.getElementById('left-sidebar').classList.remove('not-visible')
+        document.getElementById('right-sidebar').classList.remove('not-visible')
+        document.getElementById('breadcrumbs').classList.remove('not-visible')
+    }
+    catch(err) {
+        document.getElementById('docs-content').classList.remove('nodisplay');
+        document.getElementById('left-sidebar').classList.remove('not-visible')
+    }
 }
 
 const returnValue = async (event) => {
