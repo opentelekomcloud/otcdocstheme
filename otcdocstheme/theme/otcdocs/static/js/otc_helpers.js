f6f87c5991d6272b7162811fb261c1f8db9c6d40
tischrei <tino.schreiber@t-systems.com>
2022-05-06 10:01:46 +0000

    copy to clipboard implementation

--- otcdocstheme/theme/otcdocs/static/js/otc_helpers.js
+++ otcdocstheme/theme/otcdocs/static/js/otc_helpers.js
@@ -1,13 +1,29 @@
+// Display to_top-btn after scrolling 20px from top
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scroll_top.style.display = "block";
+  } else {
+    scroll_top.style.display = "none";
+  }
+}
+
+// Scroll up after clicking to_top-btn
+function to_top() {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
+}
+
+
 // Scroll-to-top implementation
 scroll_top = document.getElementById("scroll-to-top");
 
-window.onscroll = function() {
+window.onscroll = function () {
   scrollFunction()
   let footer = document.getElementById('footer');
   let scroll_height = $(window).scrollTop()
   let footer_offset = $('#footer').offset().top
   let window_height = window.innerHeight
-  if ((scroll_height + window_height) > footer_offset){
+  if ((scroll_height + window_height) > footer_offset) {
     scroll_top.classList.remove('to-top-fixed');
     scroll_top.classList.add('to-top-bottom');
   }
@@ -16,21 +32,31 @@ window.onscroll = function() {
     scroll_top.classList.remove('to-top-bottom');
   }
 
-
-
 };
 
-function scrollFunction() {
-  // Scroll after 20px from top
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scroll_top.style.display = "block";
-  } else {
-    scroll_top.style.display = "none";
-  }
+// COPY TO CLIPBOARD
+
+// add copy button to code
+function addCopyButtonToCode() {
+  var allCodeBlocksElements = $("div.highlight pre");
+
+  // For each code black element, do ...
+  allCodeBlocksElements.each(function (ii) {
+    // define a unique id for codeblock element
+    var currentId = "codeblock" + (ii + 1);
+    // Add id to codeblock
+    $(this).attr('id', currentId);
+
+    // Add button for clipboard copy and use the ID
+    var clipButton = '<button class="btn copybtn" data-clipboard-target="#' + currentId + '"><i class="fa-solid fa-copy" title="Copy to Clipboard"></i></button>';
+    $(this).after(clipButton);
+  });
+
+  // instantiate clipboardjs with class .btn
+  new Clipboard('.btn');
 }
 
-// Click on scroll-to-top button: scroll up
-function to_top() {
-  document.body.scrollTop = 0; // Safari
-  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
-}
+// Add copy buttons to code after DOM has been loaded
+$(document).ready(function () {
+  addCopyButtonToCode();
+});
