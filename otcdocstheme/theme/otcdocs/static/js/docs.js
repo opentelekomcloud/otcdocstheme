90ca816d7dba097ebce2973585398f14ecf3ed2e
Tino Schr <tino.schreiber@t-systems.com>
2022-05-18 17:22:25 +0200

    Fix bug of missing element in docsportal where no right sidebar exists. (#112)
    Fix bug of missing element in docsportal where no right sidebar exists.
    
    Reviewed-by: Artem Goncharov <Artem.goncharov@gmail.com>

--- otcdocstheme/theme/otcdocs/static/js/docs.js
+++ otcdocstheme/theme/otcdocs/static/js/docs.js
@@ -63,30 +63,35 @@ function pdfLink(currentSourceFile, pdfFileName) {
 }
 
 
+// Add magenta color to headlines of right sidebar to show document position while scrolling
 // Adding a scroll event listener to detect when page is beeing scrolled
 window.addEventListener("scroll", () => {
-    // Get all a elements except the one without a link of the right sidebar and iterate through them
-    var all_a = document.getElementById("TableOfContents").querySelectorAll("a:not([href='#'])")
 
-    for (var j = 0; j < all_a.length; j++) {
-        // Get the target headline element from the href from the all_a elements minus the # symbol
-        var elementTarget = document.getElementById(all_a[j].getAttribute("href").slice(1));
-        // If you scrolled past a headline...
-        if (window.scrollY >= elementTarget.offsetTop) {
+    // Get right sidebar and check for existance
+    let sidebar = document.getElementById("TableOfContents")
+    if (sidebar !== null) {
+        // Get all a elements except the one without a link of the right sidebar and iterate through them
+        let all_a = sidebar.querySelectorAll("a:not([href='#'])")
 
-            // get the a element on the right sidebar with the headline and all the others
-            var a = document.getElementById("TableOfContents").querySelector(`a[href='${all_a[j].getAttribute("href")}']`)
-            var not_a = document.getElementById("TableOfContents").querySelectorAll(`a:not([href='${all_a[j].getAttribute("href")}'])`)
+        for (let j = 0; j < all_a.length; j++) {
+            // Get the target headline element from the href from the all_a elements minus the # symbol
+            let elementTarget = document.getElementById(all_a[j].getAttribute("href").slice(1));
+            // If you scrolled past a headline...
+            if (window.scrollY >= elementTarget.offsetTop) {
 
-            // make the single a element magenta and all others back to grey
-            a.classList.add("color-magenta")
-            for (var i = 0; i < not_a.length; i++) {
-                not_a[i].classList.remove("color-magenta")
+                // get the a element on the right sidebar with the headline and all the others
+                let a = document.getElementById("TableOfContents").querySelector(`a[href='${all_a[j].getAttribute("href")}']`)
+                let not_a = document.getElementById("TableOfContents").querySelectorAll(`a:not([href='${all_a[j].getAttribute("href")}'])`)
+
+                // make the single a element magenta and all others back to grey
+                a.classList.add("color-magenta")
+                for (let i = 0; i < not_a.length; i++) {
+                    not_a[i].classList.remove("color-magenta")
+                }
             }
         }
     }
-
-  });
+});
 
 // Make external links open in new Tab
 // There's currently no solution to do this by CSS
