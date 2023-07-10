3bb52c89cd219f9adebb126982f59bffa26b8b18
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2023-07-10 09:19:30 +0200

    Added Option to specifyOpenSearch URL (#164)
    Added Option to specify OpenSearch URL
    
    The Default is: https://opensearch.eco.tsi-dev.otc-service.com/
    
    Reviewed-by: Vladimir Hasko <vladimirhasko@gmail.com>

--- otcdocstheme/ext.py
+++ otcdocstheme/ext.py
@@ -160,6 +160,7 @@ def _html_page_context(app, pagename, templatename, context, doctree):
         service_type = app.config.otcdocs_service_type
         service_category = app.config.otcdocs_service_category
         otcdocs_search_environment = app.config.otcdocs_search_environment
+        otcdocs_search_url = app.config.otcdocs_search_url
         _html_context_data['repository_name'] = repo_name
         _html_context_data['doc_environment'] = doc_env
         _html_context_data['doc_link'] = doc_link
@@ -169,7 +170,11 @@ def _html_page_context(app, pagename, templatename, context, doctree):
         _html_context_data['service_type'] = service_type
         _html_context_data['service_category'] = service_category
         _html_context_data['otcdocs_search_environment'] = (
-            otcdocs_search_environment)
+            otcdocs_search_environment
+        )
+        _html_context_data['otcdocs_search_url'] = (
+            otcdocs_search_url
+        )
         logger.debug('[otcdocstheme] repository_name %r', repo_name)
         if repo_name and doc_path and git_fqdn:
             _source_url = app.config.otcdocs_source_url
@@ -528,6 +533,9 @@ def setup(app):
 
     # search options
     app.add_config_value('otcdocs_search_environment', 'hc_de', 'env')
+    app.add_config_value('otcdocs_search_url',
+                         'https://opensearch.eco.tsi-dev.otc-service.com/',
+                         'env')
 
     app.set_translator('html', OTCHTML5Translator)
 
